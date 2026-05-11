5a89e839d515689bc8477951f9fcccafe178f515
aayush-aryal <67827351+aayush-aryal@users.noreply.github.com>
2026-05-11 16:40:58 -0500

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #bucket sort seems about right
+        #[[most frequent],["second most frequent"]...]
+        #since it says o(n) space compelxity im not going to create a f map
+
+        bucket=[[]for _ in range(len(nums)+1)]
+        f={}
+        for n in nums:
+            if n not in f:
+                f[n]=1
+            else:
+                f[n]+=1
+        
+        for key,v in f.items():
+            bucket[v].append(key)
+        res=[]
+        for i in range(len(bucket)-1,0,-1):
+            for n in bucket[i]:
+                if len(res)!=k:
+                    res.append(n)
+                else:
+                    break
+        return res
+        
+
+            
+
+
+
+
+
