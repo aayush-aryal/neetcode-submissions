d588eb3d45e14db3f3d1786213cbfa528234df13
aayush-aryal <67827351+aayush-aryal@users.noreply.github.com>
2026-05-17 18:01:08 -0500

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        // stack 
+        unordered_map <char,char> brackets={
+            {')','('},
+            {'}','{'},
+            {']','['}
+        };
+        vector<char> brac_stack;
+        int i=0;
+        while (i<s.size()){
+            // if its an opening bracket push onto the stack
+            if (s[i]=='(' || s[i]=='{' || s[i]=='['){
+                brac_stack.push_back(s[i]);
+            }
+            else{
+                // if the closing bracket maps to the correct opening bracket
+                if (brac_stack.size()>0 and brac_stack.back()== brackets[s[i]]){
+                    brac_stack.pop_back();
+                }
+                else{
+                    return false;
+                }
+            }
+            i+=1;
+        }
+        return brac_stack.empty();
+        
+    }
+};
